da80110d085604843c4c2397caeb85591bc754db
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-08-03 18:14:05 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,24 @@
-class UsersController < ApplicationController
-  
-  def new 
-  end 
-  
-  def create 
-    @user = User.create(user_params)
-    return redirect_to controller: 'users', action: 'new' unless @user.save
-    session[user_id  = @user.id]
-    redirect_to controller: 'welcom', action: 'home'
-  end 
-  
-  private 
-  
-  def user_params 
-    params.require(:user).permit[:name, :password, :password_confirmation]
-  end 
-  
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(name: params[:user][:name])
+
+    user = user.try(:authenticate, params[:user][:password])
+
+    return redirect_to(controller: 'sessions', action: 'new') unless user
+
+    session[:user_id] = user.id
+
+    @user = user
+
+    redirect_to controller: 'welcome', action: 'home'
+  end
+
+  def destroy
+    session.delete :user_id
+
+    redirect_to '/'
+  end
 end
