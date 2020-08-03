d1fb434a8f7a727d5ee540d1a592c5689d5442bc
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-08-03 18:14:55 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,24 @@
 class SessionsController < ApplicationController
-  
-  def new 
-  end 
-  
+  def new
+  end
+
   def create
     user = User.find_by(name: params[:user][:name])
-    user = user.try(:authenticate, params[:name][:password])
-    return redirect_to(controller: 'sessions', action: 'new')unless user 
-    session.user_id = @user.id 
-    redirect_to controller: 'welcome', action: 'home' 
-    
-  end 
-  
-  def destroy 
-    session.delete :user_id 
-    redirect_to '/' 
-  end 
-  
+
+    user = user.try(:authenticate, params[:user][:password])
+
+    return redirect_to(controller: 'sessions', action: 'new') unless user
+
+    session[:user_id] = user.id
+
+    @user = user
+
+    redirect_to controller: 'welcome', action: 'home'
+  end
+
+  def destroy
+    session.delete :user_id
+
+    redirect_to '/'
+  end
 end
